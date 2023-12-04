d28fe8dfb37ddd587f8b95f057e5f6bed9cf3233
fer0o <fer0osennin@gmail.com>
2023-12-04 03:34:43 -0600

    agregado daysi

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,11 @@
-
 function App() {
-
-
   return (
     <>
-        <h1 className="text-3xl font-bold underline">
-      Hello world!!!
-    </h1>
+      <h1 className="text-xl font-bold underline text-blue-600">
+        Hello world!!!
+      </h1>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,3 +1,4 @@
+
 /** @type {import('tailwindcss').Config} */
 export default {
   content: [    "./index.html",
@@ -5,6 +6,6 @@ export default {
   theme: {
     extend: {},
   },
-  plugins: [],
+  plugins: [require("daisyui")],
 }
 
